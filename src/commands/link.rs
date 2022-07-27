da8bfb54d83ef5f82f5b7cb25b34c8a13e2192f5
Mikkel Rasmussen <theluja@gmail.com>
2022-07-27 02:33:20 +0200

    Updated issue to struct

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -6,6 +6,7 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 use tracing::info;
 
+#[derive(PartialEq, Eq, Debug)]
 pub enum CodeLinkType {
     GitHub,
     GitHubGist,
@@ -37,13 +38,29 @@ fn match_link_code_storage(text: &str) -> Option<CodeLinkType> {
     }
 }
 
-#[derive(PartialEq, Eq, Debug)]
-pub enum IssueLinkType {
-    Issue(u64),
-    PullRequest(u64),
+#[test]
+fn parse_gh_link() {
+    let a =
+        match_link_code_storage("https://github.com/rust-lang/regex/blob/master/PERFORMANCE.md");
+    assert_eq!(a.expect("Not Found"), CodeLinkType::GitHub);
 }
 
-fn match_link_issue_or_pr(text: &str) -> Option<IssueLinkType> {
+#[derive(PartialEq, Eq, Debug)]
+pub struct Issue {
+    id: u64,
+    repo: Repo,
+    is_pr: bool,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Repo {
+    Bevy,
+    BevyBot,
+    BevyWeb,
+    Other(Option<String>), //Username/Repo
+}
+
+fn parse_issue_link(text: &str) -> Option<Issue> {
     lazy_static! {
         // From https://github.com/laundmo/gh-linker-bot/blob/main/gh_linker/cogs/code_snippets.py
         // TODO: Find out what multiline compile means
@@ -51,15 +68,15 @@ fn match_link_issue_or_pr(text: &str) -> Option<IssueLinkType> {
     }
 
     let mut a = GH_ISSUE_RE.captures_iter(text);
-    
+
     let k = a.by_ref().count();
     info!("Captured Matches: {:?}", k);
 
     for b in a {
-        for c in b.iter(){
+        for c in b.iter() {
             info!("{:?}", c);
         }
-        
+
         /*
         info!("Username {:?}", &b["Username"]);
         info!("IsUser {:?}", &b["IsUser"]);
@@ -68,19 +85,54 @@ fn match_link_issue_or_pr(text: &str) -> Option<IssueLinkType> {
         */
     }
 
-    if GH_ISSUE_RE.is_match(text)
-    {
-        Some(IssueLinkType::Issue(12))
+    if GH_ISSUE_RE.is_match(text) {
+        Some(Issue {
+            id: 123,
+            repo: Repo::Bevy,
+            is_pr: true,
+        })
     } else {
         None
     }
 }
 
+#[test]
+fn parse_bevy_issue() {
+    let a = parse_issue_link("bevy#123");
+    assert_eq!(
+        a.expect("Not Found"),
+        Issue {
+            id: 123,
+            repo: Repo::Bevy,
+            is_pr: true
+        }
+    );
+}
 
 #[test]
-fn feature() {
-    let a = match_link_issue_or_pr("bevy#123");
-    assert_eq!(a.expect("Not Found"), IssueLinkType::Issue(12));
+fn parse_bevy_web_issue() {
+    let a = parse_issue_link("web#123");
+    assert_eq!(
+        a.expect("Not Found"),
+        Issue {
+            id: 123,
+            repo: Repo::BevyWeb,
+            is_pr: true
+        }
+    );
+}
+
+#[test]
+fn parse_bevy_bot_issue() {
+    let a = parse_issue_link("bot#123");
+    assert_eq!(
+        a.expect("Not Found"),
+        Issue {
+            id: 123,
+            repo: Repo::BevyBot,
+            is_pr: true
+        }
+    );
 }
 
 #[command]
@@ -98,12 +150,19 @@ pub async fn link(ctx: &Context, msg: &Message) -> CommandResult {
         None => "",
     };
 
-
-    let res1 = match_link_issue_or_pr(&msg.content);
+    let res1 = parse_issue_link(&msg.content);
     match res1 {
-        Some(IssueLinkType::Issue(x)) => info!("Found an issue {}", x),
-        Some(IssueLinkType::PullRequest(x)) => info!("Found a pull-request {}", x),
-        None => ()
+        Some(Issue {
+            id,
+            repo,
+            is_pr: false,
+        }) => info!("Found an issue {} {:#?}", id, repo),
+        Some(Issue {
+            id,
+            repo,
+            is_pr: true,
+        }) => info!("Found a pull-request {} {:#?}", id, repo),
+        None => (),
     }
 
     if res.is_some() {
